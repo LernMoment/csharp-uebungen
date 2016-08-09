2618dc4ea9c31c3111b044f1a73c1317df651c1f
Jan Suchotzki <jan@lernmoment.de>
2016-08-09 18:02:43 +0200

    Fügt Logger in Program ein
    er wird jedoch noch nicht verwendet.

--- BibliothekenIntegrieren-NLog/BibliothekenIntegrieren-NLog/Program.cs
+++ BibliothekenIntegrieren-NLog/BibliothekenIntegrieren-NLog/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace BibliothekenIntegrieren_NLog
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
         }
