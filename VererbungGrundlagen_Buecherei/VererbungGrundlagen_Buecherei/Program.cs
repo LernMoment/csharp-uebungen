754672d540ec83d39e0007d38866de90b52de2e1
Jan Suchotzki <jan@lernmoment.de>
2016-08-10 21:16:58 +0200

    Teste KannAusgeliehenWerden von Software
    Lösung zu Aufgabe 6

--- VererbungGrundlagen_Buecherei/VererbungGrundlagen_Buecherei/Program.cs
+++ VererbungGrundlagen_Buecherei/VererbungGrundlagen_Buecherei/Program.cs
@@ -10,35 +10,18 @@ namespace VererbungGrundlagen_Buecherei
     {
         static void Main(string[] args)
         {
-            AusleihbaresMedium einMedium = new AusleihbaresMedium();
+            Software unity = new Software();
             DateTime ausleihEndeIn15Tagen = DateTime.Now.AddDays(15);
+            DateTime ausleihEndeIn6Tagen = DateTime.Now.AddDays(6);
 
-            Console.WriteLine($"Überprüfe ob das Medium bis zum {ausleihEndeIn15Tagen} ausgeliehen werden kann.");
-            if (einMedium.KannAusgeliehenWerden(ausleihEndeIn15Tagen))
+            if (unity.KannAusgeliehenWerden(ausleihEndeIn6Tagen))
             {
-                Console.WriteLine("ERFOLG: Medium kann für 15 Tage ausgeliehen werden.");
+                Console.WriteLine("ERFOLG: Software kann für 6 Tage ausgeliehen werden.");
             }
 
-            DateTime ausleihEndeIn30Tagen = DateTime.Now.AddDays(30);
-            Console.WriteLine($"Überprüfe ob das Medium bis zum {ausleihEndeIn30Tagen} ausgeliehen werden kann.");
-            if (einMedium.KannAusgeliehenWerden(ausleihEndeIn30Tagen))
+            if (!unity.KannAusgeliehenWerden(ausleihEndeIn15Tagen))
             {
-                Console.WriteLine("ERFOLG: Medium kann für 30 Tage ausgeliehen werden.");
-            }
-
-            DateTime ausleihEndeIn31Tagen = DateTime.Now.AddDays(31);
-            Console.WriteLine($"Überprüfe ob das Medium bis zum {ausleihEndeIn31Tagen} ausgeliehen werden kann.");
-            if (!einMedium.KannAusgeliehenWerden(ausleihEndeIn31Tagen))
-            {
-                Console.WriteLine("ERFOLG: Medium kann nicht für mehr als 30 Tage ausgeliehen werden.");
-            }
-
-            // Video ausleihen
-            Video tollerFilm = new Video();
-            tollerFilm.LeiheAus(DateTime.Now.AddDays(20));
-            if (!tollerFilm.KannAusgeliehenWerden(DateTime.Now.AddDays(1)))
-            {
-                Console.WriteLine("ERFOLG: Film wurde ausgeliehen und kann nicht nochmals ausgeliehen werden.");
+                Console.WriteLine("ERFOLG: Software kann nicht 15 Tage ausgeliehen werden.");
             }
 
             Console.ReadLine();
