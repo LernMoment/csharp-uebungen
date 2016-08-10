592282183141d7116e3bdd911a3790a131166b41
Jan Suchotzki <jan@lernmoment.de>
2016-08-10 18:21:30 +0200

    Überprüft ob ein Medium ausgeliehen werden kann

--- VererbungGrundlagen_Buecherei/VererbungGrundlagen_Buecherei/AusleihbaresMedium.cs
+++ VererbungGrundlagen_Buecherei/VererbungGrundlagen_Buecherei/AusleihbaresMedium.cs
@@ -8,5 +8,29 @@ namespace VererbungGrundlagen_Buecherei
 {
     public class AusleihbaresMedium
     {
+        private bool istAusgeliehen = false;
+        // TODO: JS, was ist eine sinnvolle Initialisierung?
+        private DateTime ausgeliehenBis;
+
+        public bool KannAusgeliehenWerden(DateTime gewuenschtesAusleihDatum)
+        {
+            if (istAusgeliehen)
+            {
+                return false;
+            }
+
+            if (BerechneDifferenzInTagen(DateTime.Now, gewuenschtesAusleihDatum) > 30)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private int BerechneDifferenzInTagen(DateTime von, DateTime bis)
+        {
+            TimeSpan differenz = bis.Subtract(von);
+            return differenz.Days;
+        }
     }
 }
