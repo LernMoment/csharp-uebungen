623560d3a7c1df24b9b8c6963fca210d67409a56
Jan Suchotzki <jan@lernmoment.de>
2016-08-10 21:03:40 +0200

    Fügt Video als Lösung zu Aufgabe 3 hinzu

---
+++ VererbungGrundlagen_Buecherei/VererbungGrundlagen_Buecherei/Video.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VererbungGrundlagen_Buecherei
+{
+    class Video : AusleihbaresMedium
+    {
+        public string Produzent { get; protected set; }
+    }
+}
