26b712d68c989b652746d2f10ccd8d1353023fd8
Jan Suchotzki <jan@lernmoment.de>
2017-11-28 12:47:30 +0100

    Gibt Übungsnamen aus

--- EinfachVerschluesselt/EinfachVerschluesselt/Program.cs
+++ EinfachVerschluesselt/EinfachVerschluesselt/Program.cs
@@ -6,7 +6,7 @@ namespace EinfachVerschluesselt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Text (unsicher) verschlüsseln mit ROT-13!");
         }
     }
 }
