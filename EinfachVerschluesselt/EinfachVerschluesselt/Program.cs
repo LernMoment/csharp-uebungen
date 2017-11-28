fb8f2055d66c6cb3fbf75c30510986a76ea1a6f8
Jan Suchotzki <jan@lernmoment.de>
2017-11-28 13:10:18 +0100

    Interagiert mit Benutzer
    Interaktion mit Benuther damit erstmal abgeschlossen.

--- EinfachVerschluesselt/EinfachVerschluesselt/Program.cs
+++ EinfachVerschluesselt/EinfachVerschluesselt/Program.cs
@@ -10,8 +10,18 @@ namespace EinfachVerschluesselt
             Console.WriteLine();
 
             // Text vom Benutzer eingeben lassen
+            string zuverschluesselnderText;
+            Console.WriteLine("Bitte gib den zuverschlüsselnden Text ein:");
+            zuverschluesselnderText = Console.ReadLine();
+
             // Text verschlüsseln
+            string verschluesselterText = "Verschlüsselung noch nicht implementiert!";
+
             // verschlüsselten Text ausgeben
+            Console.WriteLine();
+            Console.WriteLine($"Der verschlüsselte Text lautet: {verschluesselterText}");
+            Console.WriteLine();
+
             // Warten bis der Benutzer das Programm beendet
             Console.WriteLine("Zum Beenden bitte 'Enter' drücken");
             Console.ReadLine();
