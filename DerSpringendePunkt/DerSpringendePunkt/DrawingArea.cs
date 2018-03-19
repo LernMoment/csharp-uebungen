81f4d99015989a871e6a0da227d14494b11081de
Jan (LernMoment.de) <jan@lernmoment.de>
2018-03-19 14:29:28 +0100

    Initialer Commit für springende Punkt Übung

---
+++ DerSpringendePunkt/DerSpringendePunkt/DrawingArea.cs
@@ -0,0 +1,33 @@
+﻿
+using Lernmoment.CsharpCanvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_uebungskit
+{
+    class DrawingArea
+    {
+        private CsharpCanvas canvas;
+        public DrawingArea(CsharpCanvas aCanvas)
+        {
+            canvas = aCanvas;
+            // Vorder- und Hintergrund definieren
+            canvas.SetBackgroundColor(0x00, 0xA8, 0xC6);
+            canvas.SetForegroundColor(System.Drawing.Color.Blue);
+
+            // Draw Event abonieren
+            canvas.Draw += Canvas_Draw;
+
+            // initialisiert das canvas
+            canvas.Init();
+        }
+
+        private void Canvas_Draw()
+        {
+            // Hier können Elemente gezeichnet werden
+        }
+    }
+}
---
+++ DerSpringendePunkt/DerSpringendePunkt/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lernmoment.CsharpCanvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_uebungskit
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            CsharpCanvas canvas = new CsharpCanvas();
+            DrawingArea area = new DrawingArea(canvas);
+            canvas.Setup();
+        }
+    }
+}
